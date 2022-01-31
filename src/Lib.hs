cad7f9940cb7b0a93f81bb1af3c576233f557f25
Moritz Grauel <mgrauel@atlassian.com>
2022-02-01 09:50:39 +1100

    its alive

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,8 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module Lib
   ( goBoids,
@@ -9,28 +11,45 @@ where
 
 import Apecs
 import Apecs.Gloss
+import Apecs.TH
 import System.Exit (exitSuccess)
 
-makeWorld "World" [''Camera]
+newtype Position = Position (Float, Float) deriving (Show)
+
+newtype Velocity = Velocity (Float, Float) deriving (Show)
+
+data Boid = Boid deriving (Show)
+
+makeMapComponents [''Position, ''Velocity, ''Boid]
+makeWorld "World" [''Camera, ''Position, ''Velocity, ''Boid]
 
 type System' a = System World a
 
+makeBoid :: (Boid, Position, Velocity)
+makeBoid = (Boid, Position (0, 0), Velocity (10, 10))
+
 initialize :: System' ()
-initialize = pure ()
+initialize = do
+  newEntity_ makeBoid
+
+translate' :: Position -> Picture -> Picture
+translate' (Position (x, y)) = translate x y
 
 draw :: System' Picture
-draw = pure mempty
+draw = do
+  foldDraw (\(Boid, pos) -> translate' pos . color white . scale 1 1 $ circleSolid 3)
 
 handleEvent :: Event -> System' ()
-handleEvent (EventKey (SpecialKey KeyEsc) Down   _ _) = liftIO exitSuccess
+handleEvent (EventKey (SpecialKey KeyEsc) Down _ _) = liftIO exitSuccess
 handleEvent _ = pure ()
 
 step :: Float -> System' ()
-step dt = pure ()
+step dt = do
+  cmap $ \(Position (x,y), Velocity (vx,vy)) -> Position (x + (dt * vx), y + (dt * vy))
 
 goBoids :: IO ()
 goBoids = do
   world <- initWorld
   runWith world $ do
     initialize
-    play (InWindow "Boid" (320, 240) (10, 10)) black 60 draw handleEvent step
+    play (InWindow "Boid" (500, 500) (10, 10)) black 60 draw handleEvent step
