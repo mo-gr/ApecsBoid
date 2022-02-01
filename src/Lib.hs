a5ea27e3623e6cf775dbf2163aa4324ea77b54b9
Moritz Grauel <mgrauel@atlassian.com>
2022-02-01 20:24:35 +1100

    added explicit force

--- src/Lib.hs
+++ src/Lib.hs
@@ -21,23 +21,24 @@ newtype Position = Position Vec deriving (Show)
 
 newtype Velocity = Velocity Vec deriving (Show)
 
+newtype Force = Force Vec deriving (Show)
+
 type Vec = (Float, Float)
 
 data Boid = Boid deriving (Show)
 
 data Attractor = Attractor deriving (Show)
 
-makeMapComponents [''Position, ''Velocity, ''Boid, ''Attractor]
-makeWorld "World" [''Camera, ''Position, ''Velocity, ''Boid, ''Attractor]
+makeMapComponents [''Position, ''Velocity, ''Boid, ''Attractor, ''Force]
+makeWorld "World" [''Camera, ''Position, ''Velocity, ''Boid, ''Attractor, ''Force]
 
 type System' a = System World a
 
-makeBoid :: Float -> (Boid, Position, Velocity)
-makeBoid f = (Boid, Position (sin f * 10, cos f * 10), Velocity (0, 0))
+makeBoid :: Float -> (Boid, Position, Velocity, Force)
+makeBoid f = (Boid, Position (sin f * 10, cos f * 10), Velocity (0, 0), Force (0, 0))
 
 initialize :: System' ()
 initialize = do
-  newEntity_ (Attractor, Position (0, 0))
   forM_ [0 .. 10] $ newEntity_ . makeBoid
 
 translate' :: Position -> Picture -> Picture
@@ -55,29 +56,33 @@ handleEvent (EventKey (MouseButton LeftButton) Down (Modifiers Down _ _) (x, y))
   cmap $ \(Attractor, Position _) -> (Not @(Attractor, Position))
   newEntity_ (Attractor, Position (x, y))
 handleEvent (EventKey (MouseButton RightButton) Down _ _) = cmap $ \(Attractor, Position _) -> (Not @(Attractor, Position))
-handleEvent (EventKey (MouseButton LeftButton) Down (Modifiers Up _ _) (x, y)) = newEntity_ (Boid, Position (x, y), Velocity (0, 0))
+handleEvent (EventKey (MouseButton LeftButton) Down (Modifiers Up _ _) (x, y)) = newEntity_ (Boid, Position (x, y), Velocity (0, 0), Force (0,0))
 handleEvent _ = pure ()
 
 step :: Float -> System' ()
 step dt = do
-  centreOfMass 0.1
+  centreOfMass 50 0.1
   followOthers 50 0.125
   avoidCollisions 1
   attractor 5
+  applyForce
   clampSpeed 100
-  connectEdges
   applyVelocity dt
+  connectEdges
 
 attractor :: Float -> System' ()
 attractor fac = do
   maybeAttrac <- cfold (\_acc (Attractor, Position p) -> Just p) Nothing
   case maybeAttrac of
     Just attrac ->
-      cmap $ \(Boid, Position p, Velocity v) -> (Boid, Position p, Velocity $ vadd v (vclamp fac $ vsub attrac p))
+      cmap $ \(Boid, Position p) -> Force $ vclamp fac (attrac `vsub` p)
     Nothing -> pure ()
 
 applyVelocity :: Float -> System' ()
-applyVelocity dt = cmap $ \(Position p, Velocity v) -> Position $ vadd p (vscale dt v)
+applyVelocity dt = cmap $ \(Position p, Velocity v) -> Position $ p `vadd` vscale dt v
+
+applyForce :: System' ()
+applyForce = cmap $ \(Velocity v, Force f) -> (Velocity $ v `vadd` f, Force (0,0))
 
 clampSpeed :: Float -> System' ()
 clampSpeed mx = cmap $ \(Velocity v) -> Velocity (vclamp mx v)
@@ -90,22 +95,22 @@ connectEdges = cmap $ \case
   (Position (x, y)) | y < -250 -> Position (x, 250)
   p -> p
 
-centreOfMass :: Float -> System' ()
-centreOfMass fac = do
+centreOfMass :: Float -> Float -> System' ()
+centreOfMass centreDist fac = do
   cmapM
-    ( \(Boid, Position p, Velocity v) -> do
-        boidCount <- cfold (\acc Boid -> 1 + acc) 0
-        centre <- vscale (1 / (boidCount - 1)) <$> calcCentre p
-        pure (Boid, Position p, Velocity $ v `vadd` vscale fac (centre `vsub` p))
+    ( \(Boid, Position p, Force f) -> do
+        boidCount <- cfold (\acc (Boid, Position p') -> if vdist p p' < centreDist then 1 + acc else acc) 0
+        centre <- vscale (1 / boidCount) <$> calcCentre p
+        pure (Force $ f `vadd` vscale fac (centre `vsub` p))
     )
   where
     calcCentre :: Vec -> System' Vec
-    calcCentre pOwn = cfold (\acc (Boid, Position p) -> if p /= pOwn then vadd p acc else acc) (0, 0)
+    calcCentre pOwn = cfold (\acc (Boid, Position p) -> if p /= pOwn && vdist pOwn p < centreDist then vadd p acc else acc) (0, 0)
 
 avoidCollisions :: Float -> System' ()
-avoidCollisions fac = cmapM $ \(Boid, Position p, Velocity v) -> do
+avoidCollisions fac = cmapM $ \(Boid, Position p, Force f) -> do
   avoid <- calcAvoidance p
-  pure (Boid, Position p, Velocity $ v `vadd` vscale fac avoid)
+  pure (Force $ f `vadd` vscale fac avoid)
   where
     calcAvoidance :: Vec -> System' Vec
     calcAvoidance p = flip cfold (0, 0) $ \acc (Boid, Position p') -> case vsub p p' of
@@ -116,10 +121,10 @@ avoidCollisions fac = cmapM $ \(Boid, Position p, Velocity v) -> do
 followOthers :: Float -> Float -> System' ()
 followOthers flockDist fac = do
   cmapM
-    ( \(Boid, Position p, Velocity v) -> do
+    ( \(Boid, Position p, Velocity v, Force f) -> do
         boidCount <- cfold (\acc (Boid, Position p') -> if vdist p p' < flockDist then 1 + acc else acc) 0
-        centre <- vscale (1 / (boidCount - 1)) <$> calcVelocity p v
-        pure (Boid, Position p, Velocity $ v `vadd` vscale fac centre)
+        centre <- vscale (1 / boidCount) <$> calcVelocity p v
+        pure (Force $ f `vadd` vscale fac centre)
     )
   where
     calcVelocity :: Vec -> Vec -> System' Vec
@@ -138,7 +143,7 @@ vclamp :: Float -> Vec -> Vec
 vclamp mx (x, y) = let l = abs x + abs y in if l < mx then (x, y) else vscale (mx / l) (x, y)
 
 vdist :: Vec -> Vec -> Float
-vdist (x, y) (x', y') = abs (x - x') + abs (y -y')
+vdist (x, y) (x', y') = abs (x - x') + abs (y - y')
 
 goBoids :: IO ()
 goBoids = do
